f2c13a3fdc277b22a5c8195774e65607046fd1cf
Emma FM <emmafornermarques@gmail.com>
2019-12-06 14:22:31 +0100

    day 6

---
+++ day6.py
@@ -0,0 +1,29 @@
+import inputProcessor as iP
+import networkx as nx
+
+# We'll use a directed graph to calculate our orbits.
+# The sum of the orbits of a planet A will be the lenght of the path from A to COM
+orbits = iP.dump_list_newline("day6")
+
+# Get nodes and edges
+edges = [(x.split(")")[1],x.split(")")[0]) for x in orbits] # COM)B -> (B,COM) -> Edge from B to COM
+nodes_one = [x[0] for x in edges]
+nodes_two = [x[1] for x in edges]
+nodes = list(set().union(nodes_one, nodes_two))
+
+orbit_graph = nx.Graph()
+orbit_graph.add_nodes_from(nodes)
+orbit_graph.add_edges_from(edges)
+
+#print(nx.shortest_path_length(orbit_graph, 'COM', 'COM'))
+
+orb = 0
+for n in list(orbit_graph.nodes):
+    orb = orb + nx.shortest_path_length(orbit_graph, n, 'COM')
+
+print("Total number of orbits: ", orb)
+
+# The distance between you and Santa is again a path find problem
+
+distance = nx.shortest_path_length(orbit_graph, 'YOU', 'SAN') - 2 #-2 because we don't count YOU and SAN
+print("Orbital transfers to Santa: ", distance)
