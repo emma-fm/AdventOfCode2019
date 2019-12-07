e0df292b707d17b3030bca08327f44ffe42c32ed
Emma FM <emmafornermarques@gmail.com>
2019-12-07 16:44:02 +0100

    day 7 (not complete)

---
+++ day7.py
@@ -0,0 +1,108 @@
+import inputProcessor as iP
+
+# st is an INTEGER list!
+def calculate(st, ph, out):
+    i = 0
+    current_input = 0
+    inputs = [ph, out]
+    while i < len(st):
+        # Calculate modes
+        opcode = st[i]
+        mode = [0,0,0]
+
+        if int(st[i] / 10) != 0: # Not length 1
+            opcode = st[i] % 100 # 1002 -> 1002 % 100 = 2
+            mode[0] = int(st[i] / 100) % 10 # 1002 -> 1002 / 100 = 10 % 10 = 0
+            mode[1] = int(st[i] / 1000) % 10 # 1002 -> 1002 / 1000 = 1 % 10 = 1
+            mode[2] = int(st[i] / 10000) % 10 # 1002 -> 1002 / 10000 = 0 % 10 = 0
+        
+        # Calculate quantity of parameters (except return parameter!)
+        parameters = []
+        if opcode != 3 and opcode != 4 and opcode != 99:
+            parameters = [0,0] # two parameters
+        elif opcode == 3:
+            # Special case. Ask for input.
+            value = inputs[current_input]
+            current_input = current_input + 1
+            parameters = [value]
+        elif opcode == 4:
+            # Special case
+            parameters = [0]
+
+        # Calculate parameters for all opcodes but 3 and 99
+        if opcode != 3 and opcode != 99:
+            for j in range(0,len(parameters)):
+                if mode[j] == 0: # position mode
+                    parameters[j] = st[st[i + j + 1]]
+                else: # immediate mode
+                    parameters[j] = st[i + j + 1]
+
+        # Operate
+        if opcode == 1: # add parameters 0 and 1 and store them at direction pointed by 2
+            st[st[i + 3]] = parameters[0] + parameters[1]
+            i = i + 4
+        elif opcode == 2: # multiply parameters 0 and 2 and store them at direction pointed by 2
+            st[st[i + 3]] = parameters[0] * parameters[1]
+            i = i + 4
+        elif opcode == 3: # Store input at address of parameter 1
+            st[st[i + 1]] = parameters[0]
+            i = i + 2
+        elif opcode == 4: # Return value at address at parameter 0
+            return parameters[0]
+            i = i + 2
+        elif opcode == 5: # jump if parameter 0 != 0
+            if parameters[0] != 0:
+                i = parameters[1]
+            else:
+                i = i + 3
+        elif opcode == 6: # jump if parameter 0 == 0
+            if parameters[0] == 0:
+                i = parameters[1]
+            else:
+                i = i + 3
+        elif opcode == 7: # if parameter 0 < parameter 1, return address at 1, otherwise 0
+            if parameters[0] < parameters[1]:
+                st[st[i + 3]] = 1
+            else:
+                st[st[i + 3]] = 0
+            i = i + 4
+        elif opcode == 8: # parameter 0 == parameter 1, return address at 1, otherwise 0
+            if parameters[0] == parameters[1]:
+                st[st[i + 3]] = 1
+            else:
+                st[st[i + 3]] = 0
+            i = i + 4
+        elif opcode == 99: # Halt
+            print("Halted")
+            return
+
+states = iP.dump_list_comma("day7")
+states = [int(x) for x in states]
+states_original = states.copy()
+
+maxi = 0
+for phase in range(1234,43211): #01234 is the lowest sequence, 43210 the highest
+    phases = []
+    phases.append(int(phase / 10000))
+    phases.append(int(phase / 1000) % 10)
+    phases.append(int(phase / 100) % 10)
+    phases.append(int(phase / 10) % 10)
+    phases.append(phase % 10)
+    
+    valid = True
+    for a in phases:
+        if phases.count(a) > 1 or a > 4:
+            valid = False
+            break # We can't repeat phases or have values higher than 4
+    if not valid:
+        continue
+
+    out = 0
+    for i in range(0,5):
+        out = calculate(states, phases[i], out)
+    maxi = max(out, maxi)
+    states = states_original.copy()
+
+print("Highest signal sent: ", maxi)
+
+#### PART 2 IS NOT COMPLETED (YET?) ####
